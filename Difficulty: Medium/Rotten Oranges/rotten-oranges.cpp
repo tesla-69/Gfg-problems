1207f5fd0f7131382322135022e524c87b13f00b
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-15 01:25:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
@@ -1,75 +1,75 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution 
-{
-    public:
-    //Function to find minimum time required to rot all oranges. 
-    int orangesRotting(vector<vector<int>>& grid) {
+class Solution {
+  public:
+    // Function to find minimum time required to rot all oranges.
+    int orangesRotting(vector<vector<int>>& mat) {
         // Code here
-        vector<vector<int> > copy = grid;
-        queue< pair< pair<int,int> ,int> > q;
-        int n = grid.size();
-        int m = grid[0].size();
-        // vector<vector<int> > vis (n,vector<int>(m,0));
-        int cntf=0;
-        for(int i =0 ;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(copy[i][j] == 2) {
+        queue<pair<pair<int,int>,int>>q;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
+        for(int i= 0;i<n;i++){
+            for(int j= 0l;j<m;j++){
+                if(mat[i][j] == 2) {
+                    vis[i][j] = 2;
                     q.push({{i,j},0});
-                    // vis[i][j] = 1;
                 }
-                else if(copy[i][j] == 1) cntf++;
             }
-        } 
+        }
         int tm = 0;
-        int cnt=0;
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
             q.pop();
-            tm = max(tm,t); 
-            for(int a=-1;a<=1;a++){
-                for(int b= -1;b<=1;b++){
-                    if(abs(a) == 1 && abs(b) == 1) continue;
-
-                    int nr = r + a;
-                    int nc = c + b;
-
-                    if(nr>=0 && nr<n && nc>=0 && nc<m && copy[nr][nc] == 1 ){
-                        copy[nr][nc] = 2;
-                        // vis[nr][nc] = 1;
+            tm = max(tm,t);
+            for(int i = -1;i<=1;i++){
+                for(int j =-1;j<=1;j++){
+                    if(abs(i) == abs(j)) continue;
+                    int nr = r+i;
+                    int nc = c+j;
+                    if(nr >= 0 && nr < n && nc < m && nc>=0 
+                    && vis[nr][nc] == 0 && mat[nr][nc] == 1){
+                        vis[nr][nc] = 2;
                         q.push({{nr,nc},t+1});
-                        cnt++;
                     }
                 }
             }
         }
-        if(cnt != cntf) return -1;
+        for(int i= 0; i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 1 && vis[i][j] != 2) return -1;
+            }
+        }
         return tm;
     }
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>grid(n, vector<int>(m, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				cin >> grid[i][j];
-			}
-		}
-		Solution obj;
-		int ans = obj.orangesRotting(grid);
-		cout << ans << "\n";
-	}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> mat(n, vector<int>(m, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.orangesRotting(mat);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
