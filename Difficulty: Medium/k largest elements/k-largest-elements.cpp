6c64ec1053802eb6652cd00d52213b4d31ebba2e
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-02-17 03:22:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k largest elements/k-largest-elements.cpp
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -8,9 +8,24 @@ class Solution {
   public:
     vector<int> kLargest(vector<int>& arr, int k) {
         // Your code here
-        sort(arr.begin(),arr.end(),greater<>());
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i =0 ; i<arr.size(); i++){
+            if(pq.size() < k) {
+                pq.push(arr[i]);
+            }
+            else {
+                if(pq.top() < arr[i]) {
+                    pq.pop();
+                    pq.push(arr[i]);
+                }
+            }
+        }
         vector<int> ans;
-        for(int i =0;i<k;i++) ans.push_back(arr[i]);
+        while(!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
