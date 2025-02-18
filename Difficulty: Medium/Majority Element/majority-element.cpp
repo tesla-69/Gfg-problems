f98db6ba05f37026f6b3654f77894065cd778749
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-02-18 19:17:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        
+        int candidate=-1;
+        int votes=0;
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(votes==0)
+            {
+                candidate=arr[i];
+                votes=1;
+            }
+            else
+            {
+                if(arr[i]==candidate)
+                {
+                    votes++;
+                }
+                else
+                {
+                    votes--;
+                }
+            }
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]==candidate)
+            {
+                count++;
+            }
+        }
+        if(count>arr.size()/2)
+        {
+            return candidate;
+        }
+        return -1;
+    
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.majorityElement(a) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
