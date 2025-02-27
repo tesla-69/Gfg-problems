2850d92bbf02121391df7000d9511ed95451b29e
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-02-27 23:47:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Get Min from Stack/get-min-from-stack.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public:
+    vector<int> v1,v2;
+    Solution() {
+    }
+
+// Add an element to the top of Stack
+    void push(int x) {
+        v1.push_back(x);
+        if(v1.size() == 1)
+            v2.push_back(x);
+        else
+        {
+            if(v2.back() > x) 
+                v2.push_back(x);
+        }
+    }
+    
+    // Remove the top element from the Stack
+    void pop() {
+        int x=-1;
+        if(v1.size() > 0)
+        {
+            if(v1.back() == v2.back())
+                v2.pop_back();
+            v1.pop_back();
+        }
+    }
+    
+    // Returns top element of the Stack
+    int peek() {
+        if(v1.size() > 0)
+            return v1.back();
+        return -1;
+        }
+        
+        // Finds minimum element of Stack
+    int getMin() {
+        if(v2.size() > 0)
+            return v2.back();
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int q;
+        cin >> q;
+
+        Solution ob;
+
+        while (q--) {
+            int qt;
+            cin >> qt;
+
+            if (qt == 1) {
+                // push
+                int att;
+                cin >> att;
+                ob.push(att);
+            } else if (qt == 2) {
+                // pop
+                ob.pop();
+            } else if (qt == 3) {
+                // peek
+                cout << ob.peek() << " ";
+            } else if (qt == 4) {
+                // getMin
+                cout << ob.getMin() << " ";
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
