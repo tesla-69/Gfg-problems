682c7a41f53e525e93e7bcc448dd60c73c07b324
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-03-16 15:49:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int dp[10005];
+    int solve(int ind , vector<int> &arr){
+        if(ind == arr.size()-1) return 0;
+        if(ind >=arr.size()) return 1e7;
+        
+        if(dp[ind] != -1) return dp[ind];
+        
+        int ans = 1e7;
+        
+        for(int i = 1 ; i <= arr[ind] ; i++){
+            ans = min(ans , 1 + solve(ind + i , arr));
+        }
+        return dp[ind] = ans;
+    }
+    int minJumps(vector<int>& arr) {
+        // code here
+        memset(dp ,-1, sizeof(dp));
+        
+        int ans = solve(0 , arr);
+        if(ans == 1e7) return -1;
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, i, j;
+        vector<int> arr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.minJumps(arr) << endl << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
