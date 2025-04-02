9374cffbe5702808a800b5f46e10ff785fbe0cc1
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-04-03 00:56:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximize partitions in a String/maximize-partitions-in-a-string.cpp
+++ Difficulty: Easy/Maximize partitions in a String/maximize-partitions-in-a-string.cpp
@@ -9,22 +9,26 @@ class Solution {
   public:
     int maxPartitions(string &s) {
         // code here
-        int n=s.size();
-        unordered_map<char,int>mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[s[i]]=i;
-            
+        vector<int> fo(26, -1), lo(26, -1);
+        int n = s.size();
+        for(int i = 0; i < n; i++) {
+            if(fo[s[i] - 'a'] == -1){
+                fo[s[i] - 'a'] = i;
+                lo[s[i] - 'a'] = i;
+            } 
+            else lo[s[i] - 'a'] = i;
         }
-        int end=0,cnt=0;;
-        for(int i=0;i<n;i++)
-        {
-            end=max(mp[s[i]],end);
-            if(i==end)
-            {
-                cnt++;
+        int cnt = 0;
+        int i = 0;
+        int currs = 0;
+        int curre = lo[s[i] - 'a'];
+        while(i < n) {
+            while(i <= curre) {
+                if(lo[s[i] - 'a'] > curre) curre = lo[s[i] - 'a'];
+                i++;
             }
-            
+            cnt++;
+            curre = lo[s[i] - 'a'];
         }
         return cnt;
     }
