0604fedeb9139b326c19fbfc97edc81bc9b13598
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-04-05 17:10:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -2,48 +2,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
-  public:
-  
-    void bfs(vector<vector<char> > &grid , vector<vector<int> > &vis
-    , int row , int col , int n , int m){
-        vis[row][col] = 1;
-        queue<pair<int , int>> q;
-        q.push({row,col});
-        
-        while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
+    void bfs(int a, int b, vector<vector<char>>& grid, vector<vector<int>>& vis){
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({a,b});
+        vis[a][b] = 1;
+        while(!q.empty()) {
+            auto it = q.front();
+            int r = it.first;
+            int c = it.second;
             q.pop();
-            for(int a = -1;a<=1;a++){
-                for(int b= -1;b<=1;b++){
-                    int nrow = i + a;
-                    int ncol = j + b;
-                    if(nrow>=0 && nrow<n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
-                        q.push({nrow,ncol});
-                        vis[nrow][ncol] = 1;
-                    }
+            for(int i = -1; i <= 1; i++) {
+                for(int j = -1; j<=1; j++) {
+                    int nr = r + i;
+                    int nc = c + j;
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc]
+                        && grid[nr][nc] == 'L'){
+                            q.push({nr, nc});
+                            vis[nr][nc] = 1;
+                        }
                 }
             }
         }
-        
     }
-  
-    // Function to find the number of islands.
-    int numIslands(vector<vector<char>>& grid) {
+  public:
+    int countIslands(vector<vector<char>>& grid) {
         // Code here
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int> > vis(n , vector<int>(m , 0));
-        
+        vector<vector<int>> vis(n, vector<int>(m,0));
         int cnt = 0;
-        
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && !vis[i][j]) {
-                    bfs(grid , vis, i , j, n , m);
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(grid[i][j] == 'L' && !vis[i][j]) {
+                    bfs(i, j, grid, vis);
                     cnt++;
                 }
             }
@@ -51,6 +48,7 @@ class Solution {
         return cnt;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
@@ -66,8 +64,11 @@ int main() {
             }
         }
         Solution obj;
-        int ans = obj.numIslands(grid);
+        int ans = obj.countIslands(grid);
         cout << ans << '\n';
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
