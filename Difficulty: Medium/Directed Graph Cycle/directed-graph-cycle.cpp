d5a5aacb97b365ab2c92902a0b1dbdbe2e312450
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-04-07 22:56:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -2,79 +2,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
-  public:
-  
-    bool iscycle(int s , int vis[] , int pathvis[] , vector<int> adj[]){
-        
-        vis[s] = 1;
-        pathvis[s] = 1;
-        for(auto it : adj[s]){
+    bool dfs(vector<vector<int>> &adj , int vis[] , int pathvis[] , int node) {
+        vis[node] = 1;
+        pathvis[node] = 1;
+        for(auto it : adj[node]) {
             if(!vis[it]) {
-                if(iscycle(it,vis,pathvis,adj) == true) return true;
+                if(dfs(adj,vis,pathvis,it) == true) return true;
             }
             else if(pathvis[it] == 1) return true;
         }
-        pathvis[s] = 0;
+        pathvis[node] = 0;
         return false;
     }
-  
-    // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         int vis[V] = {0};
-        int indegree[V] = {0};
-        for(int i=0;i<V;i++){
-            for(auto it: adj[i]) {
-                indegree[it]++;
+        int pathvis[V] = {0};
+        vector<vector<int>> adj(V);
+        for(auto it : edges) {
+            adj[it[0]].push_back(it[1]);
+        }
+        for(int i = 0; i<V;i++) {
+            if(!vis[i]){
+                if(dfs(adj,vis,pathvis,i) == true) return true;
             }
         }
-        queue<int> q;
-        for(int i=0;i<V;i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
-                vis[i] = 1;
-            }
-        }
-        int cnt=0;
-        while(!q.empty()){
-            int node= q.front();
-            q.pop();
-            cnt++;
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
-            }
-        }
-        if(cnt == V) return false;
-        return true;
+        return false;
     }
 };
+
 
 //{ Driver Code Starts.
 
 int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
         int V, E;
         cin >> V >> E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back(v);
+            edges.push_back({u, v});
         }
 
         Solution obj;
-        cout << obj.isCyclic(V, adj) << "\n";
+        bool ans = obj.isCyclic(V, edges);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
     }
-
     return 0;
 }
-
 // } Driver Code Ends
