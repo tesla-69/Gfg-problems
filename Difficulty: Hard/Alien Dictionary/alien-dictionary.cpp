1486d7ff76fdf7072eae9b30d7c3bf419bddbdb7
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-04-14 15:27:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -1,104 +1,151 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-// User function Template for C++
 
-class Solution{
-    public:
-    string findOrder(string dict[], int N, int K) {
-        //code here
-       vector<int> adj[K];  // Corrected size of adj to K
-vector<int> indegree(K, 0);
-for (int i = 0; i < N - 1; i++) {
-    string s1 = dict[i];
-    string s2 = dict[i + 1];
-    int len = min(s1.length(), s2.length());
-    
-    bool foundDifference = false; // To track if we have found a difference in the current pair
-    for (int j = 0; j < len; j++) {
-        if (s1[j] != s2[j]) {
-            adj[s1[j] - 'a'].push_back(s2[j] - 'a');
-            indegree[s2[j] - 'a']++;
-            foundDifference = true;
-            break;
+class Solution {
+  public:
+    string findOrder(vector<string> &words) {
+        // code here
+        if(words.size() == 1) {
+            string ans;
+            int vis[26] = {0};
+            string s = words[0];
+            for(int i=0;i<words[0].length();i++){
+                if(!vis[s[i]-'a']) ans+=s[i];
+                vis[s[i]-'a'] = 1;
+            }
+            return ans;
         }
-    }
-
-    // Check for the prefix case
-    if (!foundDifference && s1.length() > s2.length()) {
-        return "";  // Invalid input case
-    }
-}
-
-vector<int> ans;
-queue<int> q;
-for (int i = 0; i < K; i++) {
-    if (indegree[i] == 0) q.push(i);
-}
-while (!q.empty()) {
-    int node = q.front();
-    q.pop();
-    ans.push_back(node);
-    for (auto it : adj[node]) {
-        indegree[it]--;
-        if (indegree[it] == 0) q.push(it);
-    }
-}
-string ansstr;
-for (auto it : ans) {
-    ansstr += char(it + 'a');
-}
-return ansstr;
-
+        vector<int> adj[26];
+        int n = words.size();
+        string ans;
+        int f = 1;
+        vector<int> indegree(26,-1);
+        for(int i =0;i <n-1;i++) {
+            string s1 = words[i];
+            string s2 = words[i+1];
+            
+            int len = min(s1.length(), s2.length());
+            
+            for(int i = 0;i<s1.length();i++) 
+                if(indegree[s1[i] - 'a'] == -1) indegree[s1[i]-'a'] = 0;
+            for(int i =0;i<s2.length();i++) 
+                if(indegree[s2[i] - 'a'] == -1) indegree[s2[i]-'a'] = 0;
+            
+            int update = 0;
+            
+            for(int j=0;j<len;j++){
+                if(s1[j] != s2[j]) {
+                    adj[s1[j]-'a'].push_back(s2[j]-'a');
+                    indegree[s2[j]-'a']++;
+                    update = 1;
+                    break;
+                }
+            }
+            if(update == 0 && s2.length() == len && s1.length() != s2.length()) {
+                ans = "";
+                f = 0;
+            }
+        }
+        if(!f){
+            return ans;
+        }
+        vector<int> topo;
+        queue<int> q;
+        for(int i = 0; i<26; i++) if(indegree[i] == 0) q.push(i);
+        // for(int i =0;i<26;i++) cout<<indegree[i]<<" ";
+        // cout<<endl;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        int cnt = 0;
+        for(int i =0; i< 26;i++){
+            if(indegree[i]!=-1) cnt++;
+        }
+        // for(int i =0;i<26;i++) cout<<indegree[i]<<" ";
+        // cout<<endl;
+        // cout<<"hey"<<endl;
+        if(topo.size() < cnt) return "" ;
+        for(auto ch : topo){
+            ans += ch + 'a';
+        }
+        // cout<<ans<<endl;
+        return ans;
     }
 };
 
+
+
 //{ Driver Code Starts.
-string order;
-bool f(string a, string b) {
-    int p1 = 0;
-    int p2 = 0;
-    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
-        p1 = order.find(a[i]);
-        p2 = order.find(b[i]);
-        //	cout<<p1<<" "<<p2<<endl;
+
+bool validate(const vector<string> &original, const string &order) {
+    unordered_map<char, int> mp;
+    for (const string &word : original) {
+        for (const char &ch : word) {
+            mp[ch] = 1;
+        }
+    }
+    for (const char &ch : order) {
+        if (mp.find(ch) == mp.end())
+            return false;
+        mp.erase(ch);
+    }
+    if (!mp.empty())
+        return false;
+
+    for (int i = 0; i < order.size(); i++) {
+        mp[order[i]] = i;
     }
 
-    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
-
-    return p1 < p2;
+    for (int i = 0; i < original.size() - 1; i++) {
+        const string &a = original[i];
+        const string &b = original[i + 1];
+        int k = 0, n = a.size(), m = b.size();
+        while (k < n and k < m and a[k] == b[k]) {
+            k++;
+        }
+        if (k < n and k < m and mp[a[k]] > mp[b[k]]) {
+            return false;
+        }
+        if (k != n and k == m) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Driver program to test above functions
 int main() {
-    int t;
-    cin >> t;
+    string str;
+    getline(cin, str);
+    int t = stoi(str);
     while (t--) {
-        int N, K;
-        cin >> N >> K;
-        string dict[N];
-        for (int i = 0; i < N; i++) cin >> dict[i];
-        
-        Solution obj;
-        string ans = obj.findOrder(dict, N, K);
-        order = "";
-        for (int i = 0; i < ans.size(); i++) order += ans[i];
+        getline(cin, str);
+        stringstream ss(str);
+        string curr;
+        vector<string> words;
+        while (ss >> curr)
+            words.push_back(curr);
 
-        string temp[N];
-        std::copy(dict, dict + N, temp);
-        sort(temp, temp + N, f);
+        vector<string> original = words;
 
-        bool f = true;
-        for (int i = 0; i < N; i++)
-            if (dict[i] != temp[i]) f = false;
+        Solution ob;
+        string order = ob.findOrder(words);
 
-        if(f)cout << 1;
-        else cout << 0;
-        cout << endl;
+        if (order.empty()) {
+            cout << "\"\"" << endl;
+        } else {
+            cout << (validate(original, order) ? "true" : "false") << endl;
+        }
+        cout << "~" << endl;
     }
     return 0;
 }
