e5fa514cc6f22cf8aa79894e62f934421c7663a7
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-05-26 23:44:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert in Sorted Circular Linked List/insert-in-sorted-circular-linked-list.cpp
@@ -0,0 +1,42 @@
+/* structure for a node
+class Node {
+ public:
+  int data;
+  Node *next;
+
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+}; */
+
+class Solution {
+public:
+    Node* sortedInsert(Node* head, int data) {
+        Node* newNode = new Node(data);
+
+        if (!head) {
+            newNode->next = newNode;
+            return newNode;
+        }
+
+        Node* current = head;
+
+        if (data <= head->data) {
+            while (current->next != head) {
+                current = current->next;
+            }
+            current->next = newNode;
+            newNode->next = head;
+            return newNode; 
+        }
+
+        while (current->next != head && current->next->data < data) {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+        return head;
+    }
+};
