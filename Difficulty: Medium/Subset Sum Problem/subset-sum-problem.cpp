d37d74a99613cd3aeeb929472f3c3f3df6990e4b
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-06-08 03:00:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -8,29 +8,49 @@ using namespace std;
 
 //User function template for C++
 
-class Solution{   
+class Solution{  
+    
+    bool solve(vector<int> &arr, int tar, int ind, vector<vector<int>>&dp) {
+        if(tar == 0) return true;
+        if(ind == 0) return arr[0] == tar;
+        
+        if(dp[ind][tar] != -1) return dp[ind][tar];
+        
+        bool nt = solve(arr, tar, ind - 1, dp);
+        bool t = false;
+        if(arr[ind] <= tar) t = solve(arr, tar - arr[ind], ind - 1, dp);
+        
+        return dp[ind][tar] = nt || t;
+    }
+    
+    
 public:
     bool isSubsetSum(vector<int>arr, int sum){
-        // code here 
-        int n= arr.size();
-        bool dp[n+1][sum+1];
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return solve(arr, sum, n-1, dp);
         
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0) dp[i][j] = false;
+        
+        // int n= arr.size();
+        // bool dp[n+1][sum+1];
+        
+        // for(int i=0;i<n+1;i++){
+        //     for(int j=0;j<sum+1;j++){
+        //         if(i==0) dp[i][j] = false;
             
-                if(j==0) dp[i][j] = true;
-            }
-        }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                if(arr[i-1] <= j) {
-                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
-                }
-                else dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][sum];
+        //         if(j==0) dp[i][j] = true;
+        //     }
+        // }
+        // for(int i=1;i<n+1;i++){
+        //     for(int j=1;j<sum+1;j++){
+        //         if(arr[i-1] <= j) {
+        //             dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+        //         }
+        //         else dp[i][j] = dp[i-1][j];
+        //     }
+        // }
+        // return dp[n][sum];
     }
 };
 
