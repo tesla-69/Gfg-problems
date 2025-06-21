4b1f4b166ec934b7652eac2ffe6eb789b04641b6
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-06-21 23:31:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int catchThieves(vector<char> &arr, int k) {
+        // Code here
+        int n = arr.size();
+        int i = 0, j = 0;
+        int count = 0;
+        while (i < n && j < n) {
+            while (i < n && arr[i] != 'P') i++;
+            while (j < n && arr[j] != 'T') j++;
+            if (i < n && j < n && abs(i - j) <= k) {
+                count++;
+                j++;
+                i++;
+            }
+            else if (j < n && j < i) {
+                j++;
+            }
+            else if (i < n && i < j) {
+                i++;
+            }
+        }
+        return count;
+        
+    }
+};
